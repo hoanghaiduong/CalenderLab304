39eb22ff2343f05589b6f38bb83116f75c805613
Hoàng Hải Dương <hoanghaiduong1711@gmail.com>
2022-08-09 21:30:54 +0700

    last cmit

--- route/user.routes.js
+++ route/user.routes.js
@@ -1,27 +1,41 @@
-const { authJwt } = require('../middleware');
-const { getAllUser, getUserById, deleteUser, getUserRegCaledar, createUser, get_Calender_available, userUploadAvatar, uploadAvatar, sendMail } = require("../controllers/user.controller")
-const express = require('express');
-const { db } = require('../models');
-const { updateUser } = require('../controllers/authFirebase.controller');
-const { verifyTokenFirebase, isAdminFirebase } = require('../middleware/firebaseMDL');
-const { uploadImageFirestore, Mluter } = require('../services/HandleUploadService');
-const { storageFire } = require('../config/firebase');
+const { authJwt } = require("../middleware");
+const {
+  getAllUser,
+  getUserById,
+  deleteUser,
+  getUserRegCaledar,
+  createUser,
+  get_Calender_available,
+  userUploadAvatar,
+  uploadAvatar,
+  sendMail,
+} = require("../controllers/user.controller");
+const express = require("express");
+const { db } = require("../models");
+const { updateUser } = require("../controllers/authFirebase.controller");
+const {
+  verifyTokenFirebase,
+  isAdminFirebase,
+} = require("../middleware/firebaseMDL");
+const {
+  uploadImageFirestore,
+  Mluter,
+} = require("../services/HandleUploadService");
+const { storageFire } = require("../config/firebase");
 const router = express.Router();
 const User = db.user;
 
-router.get('/getAllUser', isAdminFirebase, getAllUser);
-router.get('/getUserById/:uid', isAdminFirebase, getUserById);
-router.get('/getUserRegCaledar', verifyTokenFirebase, getUserRegCaledar);
-router.get(
-    '/get_Calender_available', get_Calender_available
-);
+router.get("/getAllUser", isAdminFirebase, getAllUser);
+router.get("/getUserById/:uid", isAdminFirebase, getUserById);
+router.get("/getUserRegCaledar", verifyTokenFirebase, getUserRegCaledar);
+router.get("/get_Calender_available", get_Calender_available);
 // router.get('/test',(req,res)=>{
 //     console.log(req.body.urlImage);
 //     const file = storageFire.file(req.body.urlImage);
 //    //get file if it exists
 //     //check file if it exists then return true delete file old
 //     //else return false
-  
+
 //     file.exists().then(async (data) => {
 //         if (data) {
 //             res.status(200).json({
@@ -40,17 +54,14 @@ router.get(
 //         });
 //     }
 //     );
-      
+
 // } )
-router.post('/sendMail',sendMail);
-router.post('/upload-avatar-user',
-    [
-        Mluter.single('image'),
-        verifyTokenFirebase,
-        uploadImageFirestore,
-        
-    ],
-    userUploadAvatar)
+router.post("/sendMail", sendMail);
+router.post(
+  "/upload-avatar-user",
+  [Mluter.single("image"), verifyTokenFirebase, uploadImageFirestore],
+  userUploadAvatar
+);
 //create user
 router.post("/createUser", createUser);
 /*Edit user */
@@ -58,5 +69,4 @@ router.put("/edit/:uid", verifyTokenFirebase, updateUser);
 /*Delete User*/
 router.delete("/delete/:uid", isAdminFirebase, deleteUser);
 
-
-module.exports = router 
+module.exports = router;
